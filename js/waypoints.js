259d4cd2c9b20cadbf03889efa1e49d8012edbcd
Kevin Yeh <kevinyeah@utexas.edu>
2015-08-22 15:17:37 -0500

    Animations for hacktx data section

---
+++ js/waypoints.js
@@ -0,0 +1,21 @@
+$('hr').waypoint(function( direction ) {
+    if (direction == "down") {
+        $(this.element).css("width", "100%");
+    } else if (direction == "up") {
+        $(this.element).css("width", "0%");
+    }
+}, {
+    triggerOnce: false,
+    offset: 'bottom-in-view'
+});
+
+$('.fadein').waypoint(function( direction ) {
+    if (direction == "down") {
+        $(this.element).css("opacity", "1");
+    } else if (direction == "up") {
+        $(this.element).css("opacity", "0");
+    }
+}, {
+    triggerOnce: false,
+    offset: 'bottom-in-view'
+});
